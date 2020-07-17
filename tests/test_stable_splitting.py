05f882a8e5f5b5a82e7f04de070a6bc79a4847b1
Alessandra Toniato <ato@zurich.ibm.com>
2020-07-17 17:37:26 +0200

    still needs to exac another file

--- tests/test_stable_splitting.py
+++ tests/test_stable_splitting.py
@@ -2,10 +2,12 @@ import pytest
 import pandas as pd
 import numpy as np
 import os.path
+
 basepath = os.path.dirname(__file__)
 
-def test_overlap_of_examples():
 
+def test_overlap_of_examples():
+    #exec(basepath + '/../scripts/example_use_pistfilter.py')
     with open(basepath + '/../data/example1/output/rxns-train.txt') as f:
         rxn_train1 = [line.strip() for line in f.readlines()]
     with open(basepath + '/../data/example2/output/rxns-train.txt') as f:
