a5aff75603761c6dae6a4fafb388af0bab6563cc
Daniel Probst <danuprobst@gmail.com>
2020-10-28 16:31:25 +0100

    Updated environment.yaml

--- setup.py
+++ setup.py
@@ -4,34 +4,32 @@ import os
 from setuptools import setup
 
 
-if os.path.exists('README.md'):
-    long_description = open('README.md').read()
+if os.path.exists("README.md"):
+    long_description = open("README.md").read()
 else:
-    long_description = '''SMILES standardizer (e.g. metal disconnector)'''
+    long_description = """SMILES standardizer (e.g. metal disconnector)"""
 
 setup(
-    name='data_preprocessor',
-    version='0.1.0',
-    author='Alessandra Toniato @ IBM',
-    author_email='ato@zurich.ibm.com',
-    py_modules=['data_preprocessor'],
-    description='Preprocessing repo for SMILEs',
+    name="data_preprocessor",
+    version="0.1.0",
+    author="Alessandra Toniato @ IBM",
+    author_email="ato@zurich.ibm.com",
+    py_modules=["data_preprocessor"],
+    description="Preprocessing repo for SMILEs",
     long_description=long_description,
-    license='MIT',
+    license="MIT",
     install_requires=[
         # 'requests==2.23.0'
-        'pandas>=1.0.1',
-        'crc64iso==0.0.2',
-        'click==7.1.2',
-        'action_sequences @ git+https://{}@github.ibm.com/rxn/action_sequences.git@tokenizer#egg=action_sequences'.format(
-        os.environ['GHE_TOKEN'])
+        "pandas>=1.0.1",
+        "crc64iso==0.0.2",
+        "click==7.1.2",
     ],
     classifiers=[
-        'Intended Audience :: Developers',
-        'Intended Audience :: Science/Research',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Topic :: Software Development :: Libraries :: Python Modules'
-    ]
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
 )
