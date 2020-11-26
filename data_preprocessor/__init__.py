5c5ad96b3b10406fe1b62c320032543e40db0849
Alessandra Toniato <ato@zurich.ibm.com>
2020-11-26 17:19:48 +0100

    added new class objects to import

--- data_preprocessor/__init__.py
+++ data_preprocessor/__init__.py
@@ -4,6 +4,7 @@ from .mixed_reaction_filter import MixedReactionFilter
 from .stable_data_splitter import StableDataSplitter
 from .preprocessor import Preprocessor
 from .augmenter import Augmenter
+from .standardizer import Standardizer, Patterns
 
 __name__ = "data_preprocessor"
 __version__ = "0.1.0"
