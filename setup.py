3ce72647646d014b5204679611de1541ef7493a8
Daniel Probst <danuprobst@gmail.com>
2020-12-03 10:35:09 +0100

    Added requirements, .travis and updated setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ else:
     long_description = """SMILES standardizer (e.g. metal disconnector)"""
 
 setup(
-    name="data_preprocessor",
+    name="rxn-data-preprocessor",
     version="0.2.0",
     author="Alessandra Toniato, Daniel Probst @ IBM",
     author_email="ato@zurich.ibm.com",
@@ -24,6 +24,13 @@ setup(
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    install_requires=[
+        "numpy == 1.19.0",
+        "pandas == 1.1.4",
+        "tabulate == 0.8.7",
+        "xxhash == 2.0.0",
+        "click == 7.1.2"
     ],
     packages=find_packages(),
     scripts=["bin/rxn-preprocess", "bin/rxn-split", "bin/rxn-tokenize"],
