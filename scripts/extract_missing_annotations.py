434a29970374be4f899740b7b6db5f0ffedda03d
Alessandra Toniato <ATO@zurich.ibm.com>
2021-09-09 17:00:41 +0200

    added helper scripts for preprocessing (#72)
    * added helper scripts for preprocessing
    
    * fixed yapf and flake8
    
    * adressing ava comments
    
    * added comment
    
    * Update scripts/remove_atom_mapping_and_fragment_info.py
    
    Co-authored-by: Alain Vaucher <AVA@zurich.ibm.com>
    
    Co-authored-by: Alessandra Toniato <ato@zhcc052@zurich.ibm.com>
    Co-authored-by: Alain Vaucher <AVA@zurich.ibm.com>

---
+++ scripts/extract_missing_annotations.py
@@ -0,0 +1,58 @@
+import click
+import ast
+import pandas as pd
+from rdkit import RDLogger
+from collections import Counter
+from rxn_chemutils.conversion import canonicalize_smiles
+
+RDLogger.DisableLog('rdApp.*')
+
+
+@click.command()
+@click.argument("input_csv", type=str, required=True)
+@click.argument("output_csv", type=str, required=True)
+@click.option("--threshold-counts", '-tc', type=int, default=5)
+def main(input_csv: str, output_csv: str, threshold_counts: int):
+    """
+    Script to count and list the number of molecules that still need an annotation.
+    The input csv file requires 2 columns:
+        'original_atom_mapped_rxn': contains the reaction SMILES and it is used to remove duplicates
+        'rxn_missing_annotations': for each line of the csv file a list of molecules requiring annotations
+    The two columns are automatically generated with the standardization script
+    """
+    df = pd.read_csv(input_csv)
+    print(df.head())
+
+    print(f"Length of dataset before duplicates removal: {len(df)}")
+    df.drop_duplicates(['original_atom_mapped_rxn'], inplace=True)
+    print(f"Length of dataset after duplicates removal: {len(df)}")
+    # converting a "list string" to a list
+    df.rxn_missing_annotations = df.rxn_missing_annotations.apply(lambda x: ast.literal_eval(x))
+
+    # save a list of canonical missing annotations
+    missing_annotations = []
+    for elem in df.rxn_missing_annotations.values:
+        if elem != []:
+            missing_annotations.extend([canonicalize_smiles(smi) for smi in elem])
+
+    c = Counter(missing_annotations)
+    print(c.most_common(10))
+    print(f"Number of missing annotations: {len(c)}")
+
+    catalysts = []
+    counts = []
+
+    for k, c in c.items():
+        if c >= threshold_counts:
+            catalysts.append(k)
+            counts.append(c)
+    print(f"Number of missing annotations with count >={threshold_counts}: {len(catalysts)}")
+
+    with open(output_csv, 'w') as f:
+        f.write("catalysts,counts\n")
+        for x in zip(catalysts, counts):
+            f.write(f"{x[0]},{x[1]}\n")
+
+
+if __name__ == "__main__":
+    main()
---
+++ scripts/remove_atom_mapping_and_fragment_info.py
@@ -0,0 +1,31 @@
+from rxn_chemutils.extended_reaction_smiles import parse_extended_reaction_smiles
+from rxn_chemutils.miscellaneous import remove_atom_mapping
+import pandas as pd
+import click
+
+
+@click.command()
+@click.option("--input_file_path", "-i", required=True)
+@click.option("--output_file_path", "-o", required=True)
+@click.option("--rxn_smiles_column", default="rxn")
+@click.option("--remove_atom_maps", default=True)
+@click.option("--fragment_bond", default="~")
+def main(
+    input_file_path: str, output_file_path: str, rxn_smiles_column: str, remove_atom_maps: bool,
+    fragment_bond: str
+):
+
+    df = pd.read_csv(input_file_path)
+    df["original_atom_mapped_rxn"] = df[rxn_smiles_column]
+    if remove_atom_maps:
+        df[rxn_smiles_column] = df[rxn_smiles_column].apply(lambda x: remove_atom_mapping(x))
+    df[rxn_smiles_column] = df[rxn_smiles_column].apply(
+        lambda x: parse_extended_reaction_smiles(x, remove_atom_maps=False).
+        to_string(fragment_bond)
+    )
+
+    df.to_csv(output_file_path, index=False)
+
+
+if __name__ == '__main__':
+    main()
