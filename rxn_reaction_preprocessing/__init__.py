9b1d32c77d1db27685d71f1c981b6fee536c2787
Travis CI Bump version <rxnid@zurich.ibm.com>
2022-05-13 11:36:19 +0000

    Bump version 0.4.5 → 0.4.6 via TravisCI (501.2) from 9a8dd8c

--- rxn_reaction_preprocessing/__init__.py
+++ rxn_reaction_preprocessing/__init__.py
@@ -13,4 +13,4 @@ from .stable_data_splitter import StableDataSplitter
 from .standardizer import Standardizer
 
 __name__ = "rxn-reaction-preprocessing"
-__version__ = "0.4.5"  # managed by bump2version
+__version__ = "0.4.6"  # managed by bump2version
